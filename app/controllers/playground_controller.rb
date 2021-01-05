80c34bdde2598fa9eb10becb7e80028e1607766c
Kay Wallaschek <wallaschek.kay@moneyforward.co.jp>
2021-01-05 09:12:14 +0900

    Add functionality to stream audio, currently only in js.

--- app/controllers/playground_controller.rb
+++ app/controllers/playground_controller.rb
@@ -3,6 +3,6 @@ class PlaygroundController < ApplicationController
     Yt.configure do |config|
       config.api_key = "AIzaSyCJk_Y4ViQjwJhhFfW-Wyd9t2dLYqWeT-U"
     end
-    @video = Yt::Video.new id: "gknzFj_0vvY"
+    @video = Yt::Video.new id: "jrsAqmh_mak"
   end
 end
