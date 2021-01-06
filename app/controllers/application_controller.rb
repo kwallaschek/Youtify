70cb42c51b7839d463f087d1555c9884367de462
Kay Wallaschek <wallaschek.kay@moneyforward.co.jp>
2021-01-06 14:03:18 +0900

    Add Internationalization to navbar. Provide translations for current strings.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def change_locale
+    puts params[:loc]
+    case params[:loc]
+    when 'de'
+      I18n.locale = :de
+    when 'us'
+      I18n.locale = :en
+    when 'jp'
+      I18n.locale = :jp
+    end
+    redirect_back fallback_location: root_path
+  end
+
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,6 +17,7 @@ require("channels")
 // const imagePath = (name) => images(name, true)
 import "bootstrap"
 import "../stylesheets/application"
+import "../stylesheets/flags.css"
 // For tooltips later
 // document.addEventListener("turbolinks:load", () => {
 //     $('[data-toggle="tooltip"]').tooltip()
